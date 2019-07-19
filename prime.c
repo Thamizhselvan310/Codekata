5ef955fe9d29cc1da7844c5b63e9ea771ee9137f
Thamizhselvan310 <52913233+Thamizhselvan310@users.noreply.github.com>
2019-07-19 11:37:15 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,i,flag=0;
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+if(n%i==0)
+{
+flag++;
+}
+}
+if(flag==1)
+{
+printf("yes");
+}
+else{
+printf("no");
+}
+}
